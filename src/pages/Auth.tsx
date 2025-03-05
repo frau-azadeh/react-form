3a47698780b757bec3cad3e062519d1ce35e2908
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-05 05:37:17 -0800

    add page Auth

---
+++ src/pages/Auth.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Auth: React.FC = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-black">Auth Page</h1>
+    </div>
+  );
+};
+
+export default Auth;
