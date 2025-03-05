d68f87a1515e44ef37b029d3c20c9d83a76adca6
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-05 08:45:20 -0800

    finish  layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer:React.FC = () => {
+  return (
+    <footer className="w-full bg-blue-950 text-white text-center py-3 mt-auto">
+        <p >@my panel</p>
+    </footer>
+)
+}
+
+export default Footer
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,26 @@
-import { ReactNode } from 'react'
-import Header from './Header';
+import { ReactNode } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
-interface LayoutProps{
-    children: ReactNode;
+interface LayoutProps {
+  children: ReactNode;
 }
-const Layout = ({children} : LayoutProps) => {
+
+const Layout = ({ children }: LayoutProps) => {
   return (
-    <div>
-        <Header/>
-        <main className='flex-1 container mx-auto p-6'>{children}</main>
-        <footer></footer>
+    <div className="min-h-screen flex flex-col bg-blue-100">
+      <Header />
+
+      <main className="flex-grow flex justify-center items-center ">
+        <div 
+          className="bg-white p-6 rounded-lg shadow-lg w-[clamp(300px,80%,1200px)] min-h-full flex flex-col"
+          >
+          {children}
+        </div>
+      </main>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
