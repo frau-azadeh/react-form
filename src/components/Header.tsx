b224be57e22ec7ad464291b734db40233fd5199f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-05 07:25:55 -0800

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,17 +4,13 @@ const Header = () => {
   return (
     <header className="w-full fixed bg-blue-950 top-0 ">
       <div className="flex justify-around items-center container mx-auto p-2">
-        {/* لوگو */}
         <h1 className="text-lx font-bold ">
           <NavLink to="/" className="hover:text-yellow-300! transition text-white! ">
             My App
           </NavLink>
         </h1>
-
-        {/* منو */}
         <nav>
           <ul className="flex space-x-7 text-lg">
-            {/** لینک ها */}
             {["/", "/auth", "/settings"].map((path, index) => (
               <li key={index}>
                 <NavLink
