75e72fe75e2ea80222b84d42276c8ea48aebbddf
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-06 09:48:04 -0800

    finish auth

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Setting from "./pages/Setting";
 import Layout from "./components/Layout";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/login" element={<Login/>}/>
             <Route path="/auth" element={<Auth />} />
             <Route path="/settings" element={<Setting />} />
             <Route path="*" element={<PageNotFound />} />
