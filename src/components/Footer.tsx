d16cb9de8d69629059eb45e570b68d28c7e86567
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-08 02:15:15 -0800

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Footer = () => {
+const Footer:React.FC = () => {
   return (
-    <div>Footer</div>
+    <footer className='w-full bg-blue-950 text-white p-3 mt-auto text-center'>
+      <p>@my panel</p>
+    </footer>
   )
 }
 
-export default Footer
+export default Footer;
