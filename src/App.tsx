3cd131e855e536d919e27417be0f74749ed7911c
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-08 01:38:30 -0800

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Setting from "./pages/Setting";
+import Login from "./pages/Login";
 import Auth from "./pages/Auth";
 import PageNotFound from "./pages/PageNotFound";
-import Setting from "./pages/Setting";
 import Layout from "./components/Layout";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
 
-function App() {
-  return (
-    <Router>
-      <ToastContainer position="top-right" autoClose={3000}/>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/dashboard" element={<Dashboard/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/settings" element={<Setting />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+function App () {
+return(
+  <Router>
+    <ToastContainer position="top-right" autoClose={3000}/>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/setting" element={<Setting/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="auth" element={<Auth/>}/>
+          <Route path="*" element={<PageNotFound/>}/>
+        </Routes>
         </Layout>
     </Router>
-  );
+    
+ 
+    
+)
 }
 
 export default App;
