3ae604fa7d3f366e5c65b301dc05b1fd3f1cdadc
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-08 02:39:26 -0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,16 @@ function App () {
 return(
   <Router>
     <ToastContainer position="top-right" autoClose={3000}/>
-      <Layout>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/dashboard" element={<Dashboard/>}/>
-          <Route path="/setting" element={<Setting/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="auth" element={<Auth/>}/>
-          <Route path="*" element={<PageNotFound/>}/>
+         <Route element={<Layout/>}>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/setting" element={<Setting/>}/>
+            <Route path="auth" element={<Auth/>}/>
+            <Route path="*" element={<PageNotFound/>}/>
+          </Route>
+          <Route path="login" element={<Login/>}/>
         </Routes>
-        </Layout>
     </Router>  
 )
 }
