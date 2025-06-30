34e56fb395b61d5314354125d605293a2f85c226
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-30 19:17:17 +0330

    update prettier schema

--- src/schemas/formSchema.ts
+++ src/schemas/formSchema.ts
@@ -44,22 +44,22 @@ export const formSchema = z.object({
     .trim()
     .regex(/^\d{10}$/, "Postal code must be a number"),
 
-age: z
-.number({ invalid_type_error: "Age must be a number"})
-.min(18, "Minimum age is 18")
-.max(65, "Maximum age is 65"),
+  age: z
+    .number({ invalid_type_error: "Age must be a number" })
+    .min(18, "Minimum age is 18")
+    .max(65, "Maximum age is 65"),
 
-gender: z.enum(["male", "female", "other"],{
-    errorMap: ()=>({message: "Gender is required"})
-}),
+  gender: z.enum(["male", "female", "other"], {
+    errorMap: () => ({ message: "Gender is required" }),
+  }),
 
-resume: z.custom<File>(
-    (file)=>file instanceof File && file.type === "application/pdf",
-    {message: "Only PDF are allowed"}
-),
-acceptTerms: z.literal(true, {
+  resume: z.custom<File>(
+    (file) => file instanceof File && file.type === "application/pdf",
+    { message: "Only PDF are allowed" },
+  ),
+  acceptTerms: z.literal(true, {
     errorMap: () => ({ message: "You must accept the terms" }),
-})
+  }),
 });
 
 export type FormSchemaType = z.infer<typeof formSchema>;
