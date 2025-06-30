d4883aa2fc807b8c33087852ee849e119d4dcc84
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-30 19:11:00 +0330

    add age schema

--- src/schemas/formSchema.ts
+++ src/schemas/formSchema.ts
@@ -10,36 +10,45 @@ export const formSchema = z.object({
     //.regex(/^[\u0600-\u06FF\s]+$/, "فقط از حروف فارسی استفاده کنید")
     //.regex(/^[a-zA-Z\s\u0600-\u06FF]+$/, "Only letters are allowed")
     .transform((val) => val.replace(/\s+/g, " ")),
-    family: z
-  .string()
-  .trim()
-  .min(2, "Family is too short")
-  .max(25, "Family is too long")
-  .regex(/^[a-zA-Z\s]+$/, "Only English letters are allowed")
-  .transform((val) =>
-    val
-      .replace(/\s+/g, " ")
-      .split(" ")
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-      .join(" ")
-  ),
-email: z
+  family: z
+    .string()
+    .trim()
+    .min(2, "Family is too short")
+    .max(25, "Family is too long")
+    .regex(/^[a-zA-Z\s]+$/, "Only English letters are allowed")
+    .transform((val) =>
+      val
+        .replace(/\s+/g, " ")
+        .split(" ")
+        .map(
+          (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(),
+        )
+        .join(" "),
+    ),
+  email: z
 
-  .string()
-  .trim()
-  .toLowerCase()
-  .regex(
-    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-    "Email format is invalid"
-  ),
-  phone:z
-  .string()
-  .trim()
-  .regex(/^09\d{9}$/,"Phone must be 11 digitis and start with 09"),
-  postalCode:z
-  .string()
-  .trim()
-  .regex(/^\d{10}$/, "Postal code must be a number")
+    .string()
+    .trim()
+    .toLowerCase()
+    .regex(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Email format is invalid",
+    ),
+  phone: z
+    .string()
+    .trim()
+    .regex(/^09\d{9}$/, "Phone must be 11 digitis and start with 09"),
+
+  postalCode: z
+    .string()
+    .trim()
+    .regex(/^\d{10}$/, "Postal code must be a number"),
+
+age: z
+.number({ invalid_type_error: "Age must be a number"})
+.min(18, "Minimum age is 18")
+.max(65, "Maximum age is 65"),
+
 });
 
 export type FormSchemaType = z.infer<typeof formSchema>;
