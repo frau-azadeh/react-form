0e021b79943241855278d0a5e49f7511c1b3dad5
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-30 18:34:06 +0330

    update name validation

--- src/schemas/formSchema.ts
+++ src/schemas/formSchema.ts
@@ -1,27 +1,14 @@
-// src/schemas/formSchema.ts
 import { z } from "zod";
 
 export const formSchema = z.object({
-  name: z.string().min(1, "Name is required"),
-  email: z.string().email("Invalid email format"),
-  phone: z
-    .string()
-    .regex(/^09\d{9}$/, "Phone must be 11 digits and start with 09"),
-  postalCode: z.string().regex(/^\d{10}$/, "Postal code must be 10 digits"),
-  age: z
-    .number({ invalid_type_error: "Age must be a number" })
-    .min(18, "Minimum age is 18")
-    .max(65, "Maximum age is 65"),
-  gender: z.enum(["male", "female", "other"], {
-    errorMap: () => ({ message: "Gender is required" }),
-  }),
-  resume: z.custom<File>(
-    (file) => file instanceof File && file.type === "application/pdf",
-    { message: "Only PDF files are allowed" },
-  ),
-  acceptTerms: z.literal(true, {
-    errorMap: () => ({ message: "You must accept the terms" }),
-  }),
-});
+    name: z.string()
+    .trim()
+    .min(2, "Name is too short")
+    .max(50, "Name is too long")
+    .regex(/^[a-zA-Z\s]+$/,"Only English letters are allowed")
+    //.regex(/^[\u0600-\u06FF\s]+$/, "فقط از حروف فارسی استفاده کنید")
+    //.regex(/^[a-zA-Z\s\u0600-\u06FF]+$/, "Only letters are allowed")
+    .transform((val)=> val.replace(/\s+/g, " ")),
+})
 
-export type FormSchemaType = z.infer<typeof formSchema>;
+export type FormSchemaType = z.infer<typeof formSchema>
