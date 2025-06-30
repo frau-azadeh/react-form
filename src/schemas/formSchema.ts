6273ab65e472cc42d6012adacaaaa15a910553e2
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-30 18:39:28 +0330

    update prettier

--- src/schemas/formSchema.ts
+++ src/schemas/formSchema.ts
@@ -1,14 +1,15 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-    name: z.string()
+  name: z
+    .string()
     .trim()
     .min(2, "Name is too short")
     .max(50, "Name is too long")
-    .regex(/^[a-zA-Z\s]+$/,"Only English letters are allowed")
+    .regex(/^[a-zA-Z\s]+$/, "Only English letters are allowed")
     //.regex(/^[\u0600-\u06FF\s]+$/, "فقط از حروف فارسی استفاده کنید")
     //.regex(/^[a-zA-Z\s\u0600-\u06FF]+$/, "Only letters are allowed")
-    .transform((val)=> val.replace(/\s+/g, " ")),
-})
+    .transform((val) => val.replace(/\s+/g, " ")),
+});
 
-export type FormSchemaType = z.infer<typeof formSchema>
+export type FormSchemaType = z.infer<typeof formSchema>;
