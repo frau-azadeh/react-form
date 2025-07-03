e88e03b3fe97c14657a4b040da1e27f49d2abce9
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-03 13:58:54 +0330

    add acceptTerms validation with zod

--- src/schemas/formSchema.ts
+++ src/schemas/formSchema.ts
@@ -17,14 +17,16 @@ export const formSchema = z.object({
     .min(2, "Family is too short")
     .max(50, "Family is too long")
     .regex(/^[a-zA-Z\s]+$/, "Only English letters are allowed")
-    .transform((val) =>
-      val
-        .replace(/\s+/g, " ")// " john doe "
-        .split(" ")// ["john", "doe"] string to object
-        .map(
-          (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(),
-        )
-        .join(" "),// "John Doe"
+    .transform(
+      (val) =>
+        val
+          .replace(/\s+/g, " ") // " john doe "
+          .split(" ") // ["john", "doe"] string to object
+          .map(
+            (word) =>
+              word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(),
+          )
+          .join(" "), // "John Doe"
     ),
   email: z
     .string()
@@ -37,26 +39,26 @@ export const formSchema = z.object({
   phone: z
     .string()
     .trim()
-    .regex(/^09\d{9}$/,"Phone must be 11 digits and start with 09"),
+    .regex(/^09\d{9}$/, "Phone must be 11 digits and start with 09"),
   postalCode: z
     .string()
     .trim()
-    .regex(/^\d{10}$/,"Postal code must be 10 "),
-  age: z
-    .number({invalid_type_error:"Age must be a number"})
-    .min(18)
-    .max(65),
-  gender: z
-    .enum(["male", "female", "other"],{
-      errorMap: ()=>({message: "Gender is required"}),
-    }),
-  resume: z 
+    .regex(/^\d{10}$/, "Postal code must be 10 "),
+  age: z.number({ invalid_type_error: "Age must be a number" }).min(18).max(65),
+  gender: z.enum(["male", "female", "other"], {
+    errorMap: () => ({ message: "Gender is required" }),
+  }),
+  resume: z
     .custom<File>(
-      (file)=>file instanceof File && file.type === "application/pdf",
-      {message: "Only PDF files are allowed"}
+      (file) => file instanceof File && file.type === "application/pdf",
+      { message: "Only PDF files are allowed" },
     )
-.optional(),
-
+    .optional(),
+  acceptTerms: z
+    .boolean()
+    .refine((val)=>val === true,{
+      message: "You must accept the terms"
+    })
 });
 
 export type FormSchemaType = z.infer<typeof formSchema>;
