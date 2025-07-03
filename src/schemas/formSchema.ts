d6b7c48d00ba7b3d320c6d8bb545b221f88dddc9
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-03 12:51:42 +0330

    add phone validation with zod

--- src/schemas/formSchema.ts
+++ src/schemas/formSchema.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-
   name: z
     .string()
     .trim()
@@ -9,31 +8,36 @@ export const formSchema = z.object({
     .max(25, "Name is too long")
     .regex(/^[a-zA-Z\s]+$/, "Only English letters are allowed")
     //.reges(/^[\u0600-\u06FF\s]+$, "فقط از حروف فارسی استفاده شود")
-   //.reges(/^[a-zA-Z\s\0u600-\u06FF]+$/,"Only letters are allowed")
-   .transform((val)=>val.replace(/\s+/g," ")),
+    //.reges(/^[a-zA-Z\s\0u600-\u06FF]+$/,"Only letters are allowed")
+    .transform((val) => val.replace(/\s+/g, " ")),
 
   family: z
     .string()
     .trim()
     .min(2, "Family is too short")
     .max(50, "Family is too long")
-    .regex(/^[a-zA-Z\s]+$/,"Only English letters are allowed")
-    .transform((val)=>
-      val 
-        .replace(/\s+/g," ")
+    .regex(/^[a-zA-Z\s]+$/, "Only English letters are allowed")
+    .transform((val) =>
+      val
+        .replace(/\s+/g, " ")
         .split(" ")
         .map(
-          (word)=> word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(),
+          (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(),
         )
         .join(" "),
     ),
-    email: z
-      .string()
-      .trim()
-      .toLowerCase()
-      .regex(     
-       /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      "Email format is invalid",)
-  })
+  email: z
+    .string()
+    .trim()
+    .toLowerCase()
+    .regex(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Email format is invalid",
+    ),
+  phone: z
+    .string()
+    .trim()
+    .regex(/^09\d{9}$/,"Phone must be 11 digits and start with 09")
+});
 
-export type FormSchemaType = z.infer<typeof formSchema>
+export type FormSchemaType = z.infer<typeof formSchema>;
