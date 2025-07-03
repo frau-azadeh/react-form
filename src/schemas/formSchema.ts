4f6fda641c6b00755f4cc754aa8b2af60f84e44f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-03 12:47:19 +0330

    add family zod

--- src/schemas/formSchema.ts
+++ src/schemas/formSchema.ts
@@ -11,6 +11,22 @@ export const formSchema = z.object({
     //.reges(/^[\u0600-\u06FF\s]+$, "فقط از حروف فارسی استفاده شود")
    //.reges(/^[a-zA-Z\s\0u600-\u06FF]+$/,"Only letters are allowed")
    .transform((val)=>val.replace(/\s+/g," ")),
-})
+
+  family: z
+    .string()
+    .trim()
+    .min(2, "Family is too short")
+    .max(50, "Family is too long")
+    .regex(/^[a-zA-Z\s]+$/,"Only English letters are allowed")
+    .transform((val)=>
+      val 
+        .replace(/\s+/g," ")
+        .split(" ")
+        .map(
+          (word)=> word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(),
+        )
+        .join(" "),
+    )
+  })
 
 export type FormSchemaType = z.infer<typeof formSchema>
