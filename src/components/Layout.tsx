0982d5a02ec361f1078850b2e09ea9201daf350d
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-06 19:53:03 +0330

    update Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-blue-100">
       <Header />
-      <main className="flex-grow flex justify-center items-center ">
+      <main className="flex-grow flex justify-center items-center mt-20 mb-5">
         <div className="bg-white p-6 rounded-lg shadow-lg w-[clamp(300px,80%,1200px)] min-h-full flex flex-col">
           <Outlet />
         </div>
