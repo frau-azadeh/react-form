912b15dd820fca703c582f3c79179da84b8da0bb
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-06 15:32:30 +0330

    add mergedSchema

---
+++ src/schemas/mergedSchema.ts
@@ -0,0 +1,8 @@
+// schemas/mergedSchema.ts
+import { z } from "zod";
+import { StepOneSchema } from "./stepOneSchema";
+import { StepTwoSchema } from "./stepTwoSchema";
+import { StepThreeSchema } from "./stepThreeSchema";
+
+export const mergedSchema = StepOneSchema.merge(StepTwoSchema).merge(StepThreeSchema);
+export type FormData = z.infer<typeof mergedSchema>;
