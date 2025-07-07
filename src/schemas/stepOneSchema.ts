73cc05ed40e957c906f48fc3085a9c290036d984
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-07 13:28:22 +0330

    create StepOneSchema

--- src/schemas/stepOneSchema.ts
+++ src/schemas/stepOneSchema.ts
@@ -1,23 +1,7 @@
 import { z } from "zod";
 
 export const StepOneSchema = z.object({
-  name: z
-    .string()
-    .trim()
-    .min(1, "Name is too short")
-    .max(25, "Name is too long")
-    .regex(/^[\u0600-\u06FF\s]+$/, "فقط حروف فارسی مجاز است"),
-  family: z
-    .string()
-    .trim()
-    .min(1, "Family name is too short")
-    .max(50, "Family name is too long")
-    .regex(/^[\u0600-\u06FF\s]+$/, "فقط حروف فارسی مجاز است"),
-  nationalCode: z.string().regex(/^\d{10}$/, "کد ملی باید 10 رقم باشد"),
-  phone: z
-    .string()
-    .regex(/^09\d{9}$/, "شماره موبایل باید 11 رقم و با 09 شروع شود"),
-  email: z.string().email("ایمیل معتبر نیست"),
-});
 
-export type StepOneSchemaType = z.infer<typeof StepOneSchema>;
+})
+
+export type StepOneSchemaType = z.infer<typeof StepOneSchema>
