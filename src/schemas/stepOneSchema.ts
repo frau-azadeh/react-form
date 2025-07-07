03b365dbac68c06931a7082cd72f66919f360c49
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-07 13:34:19 +0330

    add name validation

--- src/schemas/stepOneSchema.ts
+++ src/schemas/stepOneSchema.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
 
 export const StepOneSchema = z.object({
-
+  name: z
+    .string()
+    .trim()
+    .min(1,"نام را به درستی وارد کنید")
+    .max(25,"نام طولانی میباشد")
+    .regex(/^[\u0600-\u06FF\s]+$/,"فقط حروف فارسی مجاز است"),
 })
 
 export type StepOneSchemaType = z.infer<typeof StepOneSchema>
