4f419084488129aa4ce5d800cde93da1a3f0c30f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-07 13:50:38 +0330

    update stepOneSchema with prettier

--- src/schemas/stepOneSchema.ts
+++ src/schemas/stepOneSchema.ts
@@ -4,25 +4,20 @@ export const StepOneSchema = z.object({
   name: z
     .string()
     .trim()
-    .min(1,"نام را به درستی وارد کنید")
-    .max(25,"نام طولانی میباشد")
-    .regex(/^[\u0600-\u06FF\s]+$/,"فقط حروف فارسی مجاز است"),
+    .min(1, "نام را به درستی وارد کنید")
+    .max(25, "نام طولانی میباشد")
+    .regex(/^[\u0600-\u06FF\s]+$/, "فقط حروف فارسی مجاز است"),
   family: z
     .string()
     .trim()
-    .min(1,"نام خانوادگی را به درستی وارد کنید")
-    .max(50,"نام خانوادگی طولانی میباشد")
-    .regex(/^[\u0600-\u06FF\s]+$/,"فقط حروف فارسی مجاز است"),
-  nationalCode: z
-    .string()
-    .regex(/^\d{10}$/,"کد ملی باید 10 رقم باشد"),
+    .min(1, "نام خانوادگی را به درستی وارد کنید")
+    .max(50, "نام خانوادگی طولانی میباشد")
+    .regex(/^[\u0600-\u06FF\s]+$/, "فقط حروف فارسی مجاز است"),
+  nationalCode: z.string().regex(/^\d{10}$/, "کد ملی باید 10 رقم باشد"),
   phone: z
     .string()
-    .regex(/^09\d{9}$/,"شماره موبایل باید 11 رقم و با 09 شروع شود"),
-  email: z
-    .string()
-    .email("ایمیل معتبر نیست")
+    .regex(/^09\d{9}$/, "شماره موبایل باید 11 رقم و با 09 شروع شود"),
+  email: z.string().email("ایمیل معتبر نیست"),
+});
 
-})
-
-export type StepOneSchemaType = z.infer<typeof StepOneSchema>
+export type StepOneSchemaType = z.infer<typeof StepOneSchema>;
