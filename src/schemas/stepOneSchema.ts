38bd76a61f6d17988a2d802fc32c9a9c0b27e575
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-07 13:49:09 +0330

    add phone validation

--- src/schemas/stepOneSchema.ts
+++ src/schemas/stepOneSchema.ts
@@ -15,7 +15,11 @@ export const StepOneSchema = z.object({
     .regex(/^[\u0600-\u06FF\s]+$/,"فقط حروف فارسی مجاز است"),
   nationalCode: z
     .string()
-    .regex(/^\d{10}$/,"کد ملی باید 10 رقم باشد")
+    .regex(/^\d{10}$/,"کد ملی باید 10 رقم باشد"),
+  phone: z
+    .string()
+    .regex(/^09\d{9}$/,"شماره موبایل باید 11 رقم و با 09 شروع شود"),
+
 
 })
 
