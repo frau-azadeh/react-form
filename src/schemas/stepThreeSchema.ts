3e63d511c7dc4ac4a3b00ea7f799066b7d453b0c
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-07 20:42:09 +0330

    update checkbox validation

--- src/schemas/stepThreeSchema.ts
+++ src/schemas/stepThreeSchema.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
 export const StepThreeSchema = z.object({
-  termsAccepted: z.boolean().refine((val) => val === true, {
-    message: "باید شرایط را بپذیرید.",
-  }),
-});
+  termsAccepted: z
+    .boolean()
+    .refine((val)=> val === true,{
+      message: "باید شرایط را بپذیرید"
+    })
+})
 
-export type StepThreeSchemaType = z.infer<typeof StepThreeSchema>;
+export type StepThreeSchemaType = z.infer<typeof StepThreeSchema>
