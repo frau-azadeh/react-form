40fe7eaaef40eb7983292bd5e137654ecc4d63d7
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-18 16:49:19 +0330

    update GoBack align

--- src/components/GoBack.tsx
+++ src/components/GoBack.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Button from "./Button";
+import React from 'react'
+import Button from './Button'
 
 const GoBack: React.FC = () => {
   return (
-    <div className="flex justify-center items-center">
-      <Button onClick={() => window.history.back()}>Go Back</Button>
+    <div className='flex justify-center items-center'>
+      <Button onClick={()=> window.history.back()}>Go Back</Button>
     </div>
-  );
-};
+  )
+}
 
-export default GoBack;
+export default GoBack
